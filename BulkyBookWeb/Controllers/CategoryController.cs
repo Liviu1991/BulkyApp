d91abfb9146a562bb709dd85926694f21801039a
liviu1991 <liviu.cauia@gmail.com>
2022-03-04 13:52:12 +0000

    Add Edit and Update Category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -19,7 +19,7 @@ namespace BulkyBookWeb.Controllers
 
         //Get
         public IActionResult Create()
-        {;
+        {
             return View();
   
         }
@@ -40,6 +40,40 @@ namespace BulkyBookWeb.Controllers
             return View(obj);
         }
 
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            // Retriving Category
+            var CategoryFromDb = _db.Categories.Find(id);
+           /* var CategoryFromDbFirst = _db.Categories.FirstOrDefault(i => i.Id == id);
+            var CategoryFromDbSingle = _db.Categories.SingleOrDefault(i => i.Id == id);*/
+           if(CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Customer Error", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
 
     }
 }

4e40ee05ef06a44a63cb1f38a943031a60f77cc0
liviu1991 <liviu.cauia@gmail.com>
2022-03-09 19:37:26 +0000

    Adding images to the database

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -11,9 +11,11 @@ namespace BulkyBook.Controllers;
 public class ProductController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
-    public ProductController(IUnitOfWork unitOfWork)
+    private readonly IWebHostEnvironment _hostEnvironment;
+    public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
     {
         _unitOfWork = unitOfWork;
+        _hostEnvironment = hostEnvironment;
     }
     public IActionResult Index()
     {
@@ -58,15 +60,29 @@ public class ProductController : Controller
     //Post
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Uosert(Product obj)
+    public IActionResult Upsert(ProductVM obj, IFormFile? file)
     {
         if (ModelState.IsValid)
         {
-            _unitOfWork.Product.Update(obj);
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+            if (file != null)
+            {
+                string fileName = Guid.NewGuid().ToString();
+                var uplods = Path.Combine(wwwRootPath, @"images\products");
+                var extension = Path.GetExtension(file.FileName);
+
+                using (var fileStreams = new FileStream(Path.Combine(uplods, fileName + extension), FileMode.Create))
+                {
+                    file.CopyTo(fileStreams);
+                }
+                obj.Product.ImageUrl = @"\images\products" + fileName + extension;
+            }
+            _unitOfWork.Product.Add(obj.Product);
             _unitOfWork.Save();
-            TempData["success"] = "Product updated successfuly";
-            return RedirectToAction("Index");
-        }
+                TempData["success"] = "Product created successfuly";
+                return RedirectToAction("Index");
+            }
+        
         return View(obj);
     }
 
